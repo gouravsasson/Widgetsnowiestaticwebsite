f70abb9161e894c852572180ba8297c689639ea0
gouravsasson <sasson.gourav93@gmail.com>
2025-05-10 05:00:45 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Forkartik from "./components/Forkartik";
+import RavanVoiceAI from "./components/Ravan";
+function App() {
+  return (
+    <>
+      {/* <div className="w-screen h-screen bg-[#483d8b]"> */}
+      <RavanVoiceAI />
+      {/* </div> */}
+      {/* <Forkartik /> */}
+    </>
+  );
+}
+
+export default App;
